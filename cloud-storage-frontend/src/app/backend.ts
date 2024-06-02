c6cccaa81bc95288a75bd228aba7cc4cfa4bae99
JorgeSanchezL <jorgesanlor1221@gmail.com>
2024-06-02 19:41:58 +0200

    Frontend: executable files can now be downloaded

--- cloud-storage-frontend/src/app/backend.ts
+++ cloud-storage-frontend/src/app/backend.ts
@@ -8,16 +8,15 @@ export async function getDirectoryInfo(path: string) {
 }
 
 export async function getFileInfo(path: string) {
-    let response = await fetch(`http://localhost:5000/api/file?path=${path}`)
-    let name = path.split('/').pop()
-
-    let data = await response.text()
-    
-    // download process
-    var link = document.createElement('a')
-    let contentType = `data:${(response.headers.get('Content-Type') || 'text/plain')}`
-    console.log(data)
-    link.href = `${contentType},${encodeURIComponent(data)}`
-    link.download = name || 'file'
-    link.click()
+    fetch(`http://localhost:5000/api/file?path=${path}`)
+            .then(response => {
+                response.blob().then(blob => {
+                    let url = window.URL.createObjectURL(blob)
+                    let a = document.createElement('a')
+                    let name = path.split('/').pop()
+                    a.href = url;
+                    a.download = name || 'file'
+                    a.click()
+                });
+        });
 }
